0ac92534e7b730ff822f0e300e6cca1400c12cb0
Pedro Motta <pedromotta20@gmail.com>
2021-01-18 03:07:34 -0300

    useEffect

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 function calcFatorial(num) {
   const n = parseInt(num);
@@ -19,12 +20,23 @@ const UseEffect = (props) => {
     [number]
   );
 
+  // Ex #02
+  const [parImpar, setParImpar] = useState("Ímpar");
+
+  useEffect(
+    function () {
+      setParImpar(number % 2 === 0 ? "Par" : "Ímpar");
+    },
+    [number]
+  );
+
   return (
     <div className="UseEffect">
       <PageTitle
         title="Hook UseEffect"
         subtitle="Permite executar efeitos colaterais em componentes funcionais!"
       />
+      <SectionTitle title="Exercício #01" />
       <div className="center">
         <div>
           <span className="text">Fatorial:</span>
@@ -39,6 +51,11 @@ const UseEffect = (props) => {
           value={number}
           onChange={(e) => setNumber(e.target.value)}
         />
+      </div>
+
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        <span className="text red">{parImpar}</span>
       </div>
     </div>
   );
