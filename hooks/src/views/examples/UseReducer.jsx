a6bd75ef7a1a0b53bcbe7644035039ad3d4e3fda
Pedro Motta <pedromotta20@gmail.com>
2021-02-05 00:27:47 -0300

    useReducer

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,15 +1,90 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useReducer } from "react";
+import PageTitle from "../../components/layout/PageTitle";
 
-const UseReducer = (props) => {
-    return (
-        <div className="UseReducer">
-            <PageTitle
-                title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
-        </div>
-    )
+const initialState = {
+  cart: [],
+  products: [],
+  user: null,
+  number: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "numberAdd2":
+      return { ...state, number: state.number + 2 };
+    case "login":
+      return { ...state, user: { name: action.payload } };
+    case "numberMult7":
+      return { ...state, number: state.number * 7 };
+    case "numberDiv25":
+      return { ...state, number: state.number / 25 };
+    case "numberInt":
+      return { ...state, number: parseInt(state.number) };
+    case "numberThis":
+      return { ...state, number: action.payload };
+    default:
+      return state;
+  }
 }
 
-export default UseReducer
+const UseReducer = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="UseReducer">
+      <PageTitle
+        title="Hook UseReducer"
+        subtitle="Uma outra forma de ter estado em componentes funcionais!"
+      />
+
+      <div className="center">
+        {state.user ? (
+          <span className="text">{state.user.name}</span>
+        ) : (
+          <span className="text">Sem Usuário</span>
+        )}
+        <span className="text">{state.number}</span>
+        <div>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "login", payload: "Maria" })}
+          >
+            Login
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberAdd2" })}
+          >
+            +2
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberMult7" })}
+          >
+            x7
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberDiv25" })}
+          >
+            /25
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberInt" })}
+          >
+            Inteiro
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberThis", payload: 5 })}
+          >
+            Nº5
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseReducer;
